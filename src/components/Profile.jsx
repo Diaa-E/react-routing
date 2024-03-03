0028185c07d48c1f34069d0848994d3ed69acf78
Diaa-E <blarb21@yahoo.com>
2024-03-03 16:55:40 +0200

    Create profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+const Profile = ({ }) => {
+    return (
+        <div>
+            <h1>Profile title</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Vel perferendis nesciunt fugit enim suscipit obcaecati, 
+                maiores a officia inventore totam recusandae nihil id 
+                officiis impedit eum, nostrum, delectus porro sint.
+            </p>
+        </div>
+    )
+}
+
+export default Profile
